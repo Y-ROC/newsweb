12bd3b95a30372d4a28493932da6d331fe398785
Y-ROC <546560865@qq.com>
2018-09-25 11:33:25 +0800

    mysql和redis的配置

---
+++ main.py
@@ -0,0 +1,45 @@
+from datetime import timedelta
+
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from redis import StrictRedis
+from flask_session import Session
+
+
+class Config(object):
+    DEBUGE = True
+    # 设置数据库连接地址
+    SQLALCHEMY_DATABASE_URI = 'mysql://root:mysql@localhost:3306/newsinfo'
+    # 是否追踪数据库变化
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    REDIS_HOST = 'localhost'
+    REDIS_PORT = 6379
+    SESSION_TYPE = 'redis'
+    # 设置存储session的redis连接对象
+    SESSION_REDIS = StrictRedis(host=REDIS_HOST, port=REDIS_PORT)
+    # 设置给sessionid加密钥
+    SESSION_USE_SIGNER = True
+    # 应用密钥
+    SECRET_KEY = 'ySB+rb2WNKamAqBU4lXU0zQdf2hz+XJ80esKpQ2krNx04Hhl8n3orTg5jwhadvti'
+    # 设置session的过期时间
+    PERMANENT_SESSION_LIFETIME = timedelta(days=7)
+
+
+app = Flask(__name__)
+# 从对象加载配置信息
+app.config.from_object(Config)
+# 创建MySQL数据库连接
+db = SQLAlchemy(app)
+# 创建redis数据库连接
+sr = StrictRedis(host=Config.REDIS_HOST, port=Config.REDIS_PORT)
+# 创建session存储对象
+Session(app)
+
+
+@app.route('/')
+def index():
+    return "index"
+
+
+if __name__ == '__main__':
+    app.run()
