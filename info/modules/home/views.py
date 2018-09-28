620f8a2b81955c12d292b7dc5c6fa8db14d29d92
Y-ROC <546560865@qq.com>
2018-09-28 16:02:58 +0800

    新闻详情页面

--- info/modules/home/views.py
+++ info/modules/home/views.py
@@ -1,8 +1,8 @@
-from info import sr
-from info.constants import CLICK_RANK_MAX_NEWS
+from info.constants import CLICK_RANK_MAX_NEWS, HOME_PAGE_MAX_NEWS
 from info.models import User, News, Category
 from info.modules.home import home_blu
-from flask import render_template, current_app, session
+from flask import render_template, current_app, session, request, jsonify
+from info.utils.response_code import RET, error_map
 
 
 # 2.使用蓝图注册路由
@@ -23,16 +23,52 @@ def index():
     except BaseException as e:
         current_app.logger.error(e)
     news_list = [news.to_basic_dict() for news in news_list]
-    #查询所有分类信息
-    categories=[]
+    # 查询所有分类信息
+    categories = []
     try:
-        categories= Category.query.all()
+        categories = Category.query.all()
     except BaseException as e:
         current_app.logger.error(e)
-    return render_template('index.html', user=user, news_list=news_list,categories=categories)
+    return render_template('index.html', user=user, news_list=news_list, categories=categories)
 
 
 # 设置网站小图标
 @home_blu.route('/favicon.ico')
 def favicon():
     return current_app.send_static_file('news/favicon.ico')
+
+
+# 获取新闻列表
+@home_blu.route('/get_news_list')
+def get_news_list():
+    # 获取参数
+    cid = request.args.get('cid')  # 分类id
+    cur_page = request.args.get('cur_page')  # 当前页码
+    per_count = request.args.get('per_count', HOME_PAGE_MAX_NEWS)  # 每页条数
+    # 校验参数
+    if not all([cid, cur_page, per_count]):
+        return jsonify(errno=RET.PARAMERR, errmsg=error_map[RET.PARAMERR])
+    # 格式转换
+    try:
+        cid = int(cid)
+        cur_page = int(cur_page)
+        per_count = int(per_count)
+    except BaseException as e:
+        current_app.logger.error(e)
+        return jsonify(errno=RET.PARAMERR, errmsg=error_map[RET.PARAMERR])
+    # 判断分类id是否等于1  '最新'是所有新闻一起排列,不包括具体新闻
+    filter_list = []
+    if cid != 1:
+        filter_list.append(News.category_id == cid)
+    # 根据参数查询目标新闻
+    try:
+        pn = News.query.filter(*filter_list).order_by(News.create_time.desc()).paginate(cur_page, per_count)
+    except BaseException as e:
+        current_app.logger.error(e)
+        return jsonify(errno=RET.DBERR, errmsg=error_map[RET.DBERR])
+    data = {
+        "news_list": [news.to_basic_dict() for news in pn.items],
+        "total_page": pn.pages
+    }
+    # 将新闻包装为json并返回
+    return jsonify(errno=RET.OK, errmsg=error_map[RET.OK], data=data)
--- info/modules/news/views.py
+++ info/modules/news/views.py
@@ -1,41 +1,16 @@
-from flask import request, jsonify, current_app
-from info.constants import HOME_PAGE_MAX_NEWS
+from flask import current_app, abort, render_template
+
 from info.models import News
 from info.modules.news import news_blu
-from info.utils.response_code import RET, error_map
 
 
-# 获取新闻列表
-@news_blu.route('/get_news_list')
-def get_news_list():
-    # 获取参数
-    cid = request.args.get('cid')  # 分类id
-    cur_page = request.args.get('cur_page')  # 当前页码
-    per_count = request.args.get('per_count', HOME_PAGE_MAX_NEWS)  # 每页条数
-    # 校验参数
-    if not all([cid, cur_page, per_count]):
-        return jsonify(errno=RET.PARAMERR, errmsg=error_map[RET.PARAMERR])
-    # 格式转换
+# 详情页面
+@news_blu.route('/<int:news_id>')
+def news_detail(news_id):
     try:
-        cid = int(cid)
-        cur_page = int(cur_page)
-        per_count = int(per_count)
+        news = News.query.get(news_id)
     except BaseException as e:
         current_app.logger.error(e)
-        return jsonify(errno=RET.PARAMERR, errmsg=error_map[RET.PARAMERR])
-    # 判断分类id是否等于1  '最新'是所有新闻一起排列,不包括具体新闻
-    filter_list = []
-    if cid != 1:
-        filter_list.append(News.category_id == cid)
-    # 根据参数查询目标新闻
-    try:
-        pn = News.query.filter(*filter_list).order_by(News.create_time.desc()).paginate(cur_page, per_count)
-    except BaseException as e:
-        current_app.logger.error(e)
-        return jsonify(errno=RET.DBERR, errmsg=error_map[RET.DBERR])
-    data = {
-        "news_list": [news.to_basic_dict() for news in pn.items],
-        "total_page": pn.pages
-    }
-    # 将新闻包装为json并返回
-    return jsonify(errno=RET.OK, errmsg=error_map[RET.OK], data=data)
+        return abort(404)
+    # 将数据传入模板进行模板渲染
+    return render_template('detail.html', news=news.to_dict())
