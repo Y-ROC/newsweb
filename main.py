63027c5d9dab770964c4e0361118dd2b3abf500e
Y-ROC <546560865@qq.com>
2018-10-10 19:18:58 +0800

    生成管理员命令

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from flask import current_app
 from flask_script import Manager
 from flask_migrate import MigrateCommand
 from info import create_app
@@ -7,6 +8,32 @@ app = create_app('dev')
 mgr = Manager(app)
 # 添加迁移命令
 mgr.add_command('mc', MigrateCommand)
+
+
+# 生成管理员命令,先封装成函数.再将函数包装成命令
+@mgr.option("-u", dest="username")
+@mgr.option("-p", dest="password")
+def create_superuser(username, password):
+    if not all([username, password]):
+        print('参数不足')
+        return
+    from info.models import User
+    from info import db
+    user = User()
+    user.mobile = username
+    user.password = password
+    user.nick_name = username
+    user.is_admin = True
+    # 添加到数据库
+    try:
+        db.session.add(user)
+        db.session.commit()
+    except BaseException as e:
+        current_app.logger.error(e)
+        db.session.rollback()
+        print('数据库操作错误')
+    print("管理员创建成功")
+
 
 if __name__ == '__main__':
     mgr.run()
